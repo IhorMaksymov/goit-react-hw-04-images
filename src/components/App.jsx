2a24213f70adeb16f1437135dbe50a74a5127e00
IhorMaksymov <bonefadze0ne@gmail.com>
2022-12-16 07:20:51 +0200

    load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,19 +14,22 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [imageName, setImageName] = useState('');
   const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState('');
 
   useEffect(() => {
+    const prePage = 12;
     if (imageName) {
       setLoading(true);
-      API(imageName, page)
+      API(imageName, page, prePage)
         .then(res => {
-          setArrayItems((prev) => { return [...prev, ...res] })
+          setArrayItems((prev) => { return [...prev, ...res.hits] })
+          setTotalPage(res.totalHits)
           setLoading(false)
         })
     }
   }, [imageName, page])
 
-  const handleSubmitForm = ({imageName}) => {
+  const handleSubmitForm = ({ imageName }) => {
     setImageName(imageName)
     setPage(1)
     setArrayItems([])
@@ -34,17 +37,20 @@ const App = () => {
   
   return (
     <Box
-        display='flex'
-        flexDirection='column'
-        alignItems='center'
-        as='main'
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      as='main'
     >
       <SearchBar onSubmit={handleSubmitForm} />
       {loading && <Loader />}
       {imageName &&
         <>
-        <ImageGallery items={arrayItems} />
-        {loading ? <Loader /> : <Button loadMore={() => setPage(page + 1)} />}
+          <ImageGallery items={arrayItems} />
+          {totalPage !== arrayItems.length && (
+            <Button loadMore={() => setPage(page + 1)} />
+          )}
+          {loading && <Loader />}
         </>}
       <GlobalStyle />
     </Box>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,18 +3,18 @@ import axios from 'axios';
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '29118139-f0fadce498ed45b93da5b0f44';
 
-const getMaterial = async (name, page) => {
+const getMaterial = async (name, page, prePage) => {
     const options = new URLSearchParams({
         key: API_KEY,
         q: name,
         image_type: 'photo',
         orientation: 'horizontal',
         page: page,
-        per_page: 12,
+        per_page: prePage,
     })
 
     const response = await axios.get(`${API_URL}?${options}`);
-    return response.data.hits;
+    return response.data;
 }
 
 export default getMaterial;
